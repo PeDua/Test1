ac484968de25122ae5b035938c44f329212d3239
Cntt21 <Admin21@Cntt21s-iMac.local>
2018-04-06 10:26:10 +0700

    Initial Commit

---
+++ ttt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ttt
+//
+//  Created by Cntt21 on 4/6/18.
+//  Copyright © 2018 Cntt21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
